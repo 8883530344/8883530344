2a04ec304c65aaa191fe9419c0ff7d67809d3b9d
8883530344 <109344624+8883530344@users.noreply.github.com>
2022-09-16 15:17:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var cv={
+    "name":"Dinesh",
+    "age" :"26",
+    "education":"MBA",
+    "email":"dineshosthi2310@gmail.com",
+    "language":"tamil"
+}
+for (var i in cv){
+console.log(cv[i])
+
+};
